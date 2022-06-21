331ce614bb293857a4c55c968ace02512faccf1b
Vincenzofdg <vfdgiacomo@gmail.com>
2022-06-20 21:34:25 -0300

    backend ==== frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import apiMethod from "./services/api";
 
 function App() {
+	const api = apiMethod('GET', '/people').then(i => console.log(i.data))
+	console.log(api);
 
-  const test = apiMethod('GET', '/')
-  console.log(test)
+  // axios.get('http://localhost:3001/').then(i => console.log(i.data));
 
   return (
     <>
